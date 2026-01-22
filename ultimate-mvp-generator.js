dc460b37531b4d58806218b352716b413d885c11
IvanEfimov-StrategixAI <ivan.algobait@gmail.com>
2026-01-22 19:03:49 +0300

    Add MVP generator and Vercel config

--- ultimate-mvp-generator.js
+++ ultimate-mvp-generator.js
@@ -1,6 +1,15 @@
-// Создайте простой модуль
-module.exports = {
-  generateMVP: function() {
-    return "MVP Generator работает!";
-  }
-};
+// Ultimate MVP Generator 
+module.exports = { 
+  generateMVP: async function(businessIdea, options = {}) { 
+    console.log('MVP Generator called for:', businessIdea); 
+    return { 
+      success: true, 
+      mvp: 'Mock MVP generated successfully', 
+      features: ['Feature 1', 'Feature 2', 'Feature 3'], 
+      timeline: '4 weeks' 
+    }; 
+  }, 
+  validateIdea: function(idea) { 
+    return { valid: true, score: 85 }; 
+  } 
+}; 
